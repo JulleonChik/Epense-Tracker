c0176409d1da2e8716dc05335c406dbd586b1395
Julleon <julleonchik@gmail.com>
2024-07-31 20:01:23 +0300

    init

---
+++ app/src/main/java/com/leon/expensetracker/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.leon.expensetracker
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen() {
+
+}
+
+@Composable
+@Preview(device = "id:pixel_6_pro")
+fun PreviewHomeScreen() {
+    HomeScreen()
+}
